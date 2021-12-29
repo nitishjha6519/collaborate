d7bd7f25a6a5e99f0b18dc54279a811d29c2f7e0
Nitish Jha <nitishjha6519@gmail.com>
2021-12-29 20:08:48 +0530

    apply page added

--- app.js
+++ app.js
@@ -1,52 +1,90 @@
 const express=require("express");
 const mongoose=require("mongoose");
+const bodyParser= require("body-parser");
 
 mongoose.connect("mongodb://localhost:27017/collabShipDB");
+
+
 const collabShipSchema= new mongoose.Schema({
 	projectName: String,
 	type: String,
 	date: Date
 
 });
+
+
+const signupsSchema= new mongoose.Schema({
+	name: String,
+	gitProfile: String
+
+});
+
 const boxes=mongoose.model("boxes",collabShipSchema );   //collections called nameEmail
-
-// const item1=new boxes ({
-// 	projectName: "Apple",
-// 	type: "Backend",
-// 	date: "2021-12-27",
-// });
-// item1.save();
+var arr=[];
 
 
- boxes.find(function(err,docs){
-if(err){
-	console.log(err);
-}
-else{
-
-	// console.log(docs);
-	docs.forEach(function(doc){
-		console.log(doc.projectName);
+const signups=mongoose.model("signups",signupsSchema );   //collections called nameEmail
+var signupsArr=[];
 
 
-	});
-}
+
+boxes.find(function(err,docs){ 
+	if(err){
+		console.log(err);
+	} else{ 
+		docs.forEach(function(doc){
+			arr.push(doc.projectName) ;
+		}); 
+	} 
+	console.log(arr);
+// console.log(docs);
 });
 
 
 
 const app=express();
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static("Public"));
 
 app.get("/", function(req,res){
-res.render("index");
+	res.render("index" , {arr: arr});
+});
+
+app.get("/apply", function(req,res){
+	res.sendFile(__dirname + "/Apply.html");
+});
+
+app.post("/apply", function(req,res){
+	var userName= req.body.name;
+
+
+	const user= new signups({
+		name: req.body.name,
+		gitProfile: req.body.gitProfile,
+	});
+	user.save(function(err){
+		if(err){
+			console.log(err);
+		}
+		else{
+			
+          res.render("welcome");
+          
+
+			
+			
+		}
+	});
+	
+
 });
 
 
 
 
+
 app.listen(3000, function(){
-    console.log("server is running at port 3000");
+	console.log("server is running at port 3000");
 });
 
